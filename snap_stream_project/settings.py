8abde72ed7da1a39dc61f3687e95893e1b245176
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2025-09-12 12:46:55 +0300

    i think i fixed the settings error

--- snap_stream_project/settings.py
+++ snap_stream_project/settings.py
@@ -95,9 +95,7 @@ WSGI_APPLICATION = 'snap_stream_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL")),
-    'conn_max_age': 600,
-    'ssl_require': True
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"), conn_max_age=600, ssl_require=True)
 }
 
 REST_FRAMEWORK = {
