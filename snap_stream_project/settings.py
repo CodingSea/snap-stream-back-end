719ff49fae61ce2aefdacb50d23b820fbf572716
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2025-09-12 12:52:31 +0300

    made a small chnage to settings

--- snap_stream_project/settings.py
+++ snap_stream_project/settings.py
@@ -93,9 +93,12 @@ WSGI_APPLICATION = 'snap_stream_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"), conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 REST_FRAMEWORK = {
